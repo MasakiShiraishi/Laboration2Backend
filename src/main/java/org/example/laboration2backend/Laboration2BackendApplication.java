9e207635d5730e5028c9a3d40fea28dad10223c6
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2024-11-13 14:42:38 +0100

    Initial commit

---
+++ src/main/java/org/example/laboration2backend/Laboration2BackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.laboration2backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboration2BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Laboration2BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/laboration2backend/Laboration2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.laboration2backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboration2BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
