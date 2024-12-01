47a397dbc03141e0cf1633032e3dba9b13420ba7
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2024-12-01 11:06:21 +0100

    added retry function to PlayGroundController

--- src/main/java/org/example/laboration2backend/playground/PlayGroundController.java
+++ src/main/java/org/example/laboration2backend/playground/PlayGroundController.java
@@ -1,8 +1,12 @@
 package org.example.laboration2backend.playground;
 
+import lombok.extern.slf4j.Slf4j;
 import org.example.laboration2backend.dto.PlaceDto;
 import org.example.laboration2backend.entity.Playground;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -12,6 +16,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api")
 @CrossOrigin
+@Slf4j
 public class PlayGroundController {
 
     private final PlaygroundService playgroundService;
@@ -21,6 +26,7 @@ public class PlayGroundController {
     }
 
     @GetMapping("/playgrounds")
+    @Retryable(value = {Exception.class}, maxAttempts = 3, backoff = @Backoff(delay = 2000))
     public List<APlace> allPlaygrounds(@RequestParam(required = false) Map<String, String> queryParams) {
         return playgroundService.all().stream()
                 .map(p ->
@@ -32,6 +38,7 @@ public class PlayGroundController {
 
 
     @GetMapping("/playgrounds/search")
+    @Retryable(value = {Exception.class}, maxAttempts = 3, backoff = @Backoff(delay = 2000))
     public List<PlaceDto> searchPlaygrounds(@RequestParam Double lat,
                                             @RequestParam Double lon,
                                             @RequestParam Double radius) {
@@ -43,8 +50,13 @@ public class PlayGroundController {
     }
 
     @PostMapping("/playgrounds")
+    @Retryable(value = {Exception.class}, maxAttempts = 3, backoff = @Backoff(delay = 2000))
     public ResponseEntity<Void> addPlayground(@RequestBody Location location) {
         Playground p = playgroundService.createNewPlayGround(location.lat(), location.lon());
         return ResponseEntity.created(URI.create("/playgrounds/" + p.getId())).build();
     }
+
+    @Recover
+    public ResponseEntity<Void> recover(Exception e) {
+        log.error("Error during operation: {}", e.getMessage()); return ResponseEntity.status(500).build(); }
 }
